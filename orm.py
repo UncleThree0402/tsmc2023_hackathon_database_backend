6aaaadd3ff9d06aa958e3d394d6590f559b3706e
JoeySmith1103 <stu30405@gmail.com>
2023-02-10 12:54:31 +0800

    add insert user and insert car functions

--- orm.py
+++ orm.py
@@ -30,9 +30,9 @@ class Database:
     # insert into parking_space table
     def insert_parking_lots(self, json_input):
         for one in json_input['data']:
-            ParkingLotName = one['ParkingLotName']
-            AvailableSpots = one['AvailableSpots']
-            LongitudeAndLatitude = one['LongitudeAndLatitude']
+            ParkingLotName = one['parkingLotName']
+            AvailableSpots = one['availableSpots']
+            LongitudeAndLatitude = one['longitudeAndLatitude']
             # insert statement
             insert_stmt = sqlalchemy.text("INSERT INTO ParkingLots (ParkingLotName, LongitudeAndLatitude, AvailableSpots)"
             + "VALUES (:ParkingLotName, NULLIF(:LongitudeAndLatitude, ''), :AvailableSpots)")
@@ -44,15 +44,88 @@ class Database:
                     LongitudeAndLatitude=LongitudeAndLatitude,
                     AvailableSpots=AvailableSpots
                 )
+
     # parking lots input
     # obj = {
     # "data": [
     #     {
-    #         "ParkingLotName": "E",
-    #         "LongitudeAndLatitude": "",
-    #         "AvailableSpots": 300
+    #         "parkingLotName": "E",
+    #         "longitudeAndLatitude": "",
+    #         "availableSpots": 300
     #     }
     # ]
+    # }
+
+    # insert into users table
+    def insert_users(self, json_input):
+        for one in json_input['data']:
+            UserID = one['userID']
+            Account = one['account']
+            Password = one['password']
+            Email = one['email']
+            PhoneNumber = one['phoneNumber']
+            Identity = one['identity']
+
+            # insert statement
+            insert_stmt = sqlalchemy.text("INSERT INTO Users (UserID, Account, Password, Email, PhoneNumber, Identity)"
+            + "VALUES (:UserID, NULLIF(:Account, ''), NULLIF(:Password, ''), NULLIF(:Email, ''), NULLIF(:PhoneNumber, ''), :Identity)")
+
+            with self.pool.connect() as db_conn:
+                db_conn.execute(
+                    insert_stmt,
+                    UserID=UserID,
+                    Account=Account,
+                    Password=Password,
+                    Email=Email,
+                    PhoneNumber=PhoneNumber,
+                    Identity=Identity
+                )
+
+    # user input
+    # obj = {
+    #     "data": [
+    #         {
+    #             "userID": "abc123",
+    #             "account": "lebromjames",
+    #             "password": "hahaiamidiot",
+    #             "email": "stu30405@gmail.com",
+    #             "phoneNumber": "0935774796",
+    #             "identity": "User"   
+    #         }
+    #     ]
+    # }
+
+    # insert into cars table
+    def insert_cars(self, json_input):
+        for one in json_input['data']:
+            LicensePlate = one['licensePlate']
+            UserID = one['userID']
+            ParkingStartTime = one['parkingStartTime']
+            ParkingEndTime = one['parkingEndTime']
+
+        # insert statement
+        insert_stmt = sqlalchemy.text("INSERT INTO Cars (LicensePlate, UserID, ParkingStartTime, ParkingEndTime)"
+        + "VALUES (:LicensePlate, :UserID, NULLIF(:ParkingStartTime, ''), NULLIF(:ParkingEndTime, ''))")
+
+        with self.pool.connect() as db_conn:
+                db_conn.execute(
+                    insert_stmt,
+                    LicensePlate=LicensePlate,
+                    UserID=UserID,
+                    ParkingStartTime=ParkingStartTime,
+                    ParkingEndTime=ParkingEndTime
+                )
+
+    # car input
+    # obj = {
+    #     "data": [
+    #         {
+    #             "licensePlate": "1234ZZ",
+    #             "userID": "abc123",
+    #             "parkingStartTime": "2023-01-01 08:10:00",
+    #             "parkingEndTime": "2023-01-01 08:44:00"
+    #         }
+    #     ]
     # }
 
 
